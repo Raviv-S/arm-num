1068efc0b7ca8ce31fda25a573a8b8246f9dd70c
Raviv S <raviv.schaffer@gmail.com>
2020-04-28 14:53:55 +0300

    Fixed a minor comment at the arm_calc() function

--- arm_num.py
+++ arm_num.py
@@ -1,19 +1,12 @@
 def arm_calc(inp_num):
     """
-    Does the Armstrong calculation - Sum of each digit to the power of number of digit in the number.
+    Calculates the sum of each digit to the power of number of digit in the number.
     :param num: int
     :return: calculated int
     """
-    num_digit_list = list(str(inp_num))
-    num_digit_list = [int(digit) for digit in num_digit_list]
+    num_digit_list = list(map(int, str(inp_num)))
     num_len = len(num_digit_list)
-    sum_result = 0
-
-    # Calculating the sum of digits
-    for digit in num_digit_list:
-        sum_result += digit ** num_len
-
-    return sum_result
+    return sum([digit ** num_len for digit in num_digit_list])
 
 
 def armstrong_numbers(checked_num):
@@ -30,7 +23,7 @@ def armstrong_numbers(checked_num):
 
 
 def main():
-    print(armstrong_numbers(153))
+    print(armstrong_numbers(99999))
 
 
 if __name__ == '__main__':
